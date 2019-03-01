abfcf2fd89c5f3db46defa0551a6d5427149d5fb
Nate Jonah <nate@natejonah.com>
2019-03-01 23:30:06 +0000

    Set view engne to react and render index view for root route

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const fetch = require("node-fetch");
 
-app.get('/', (req, res) => res.send("Quiz"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
