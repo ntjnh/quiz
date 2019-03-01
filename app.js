04083f6728614b59c8b333351e0575f9bb724c0d
Nate Jonah <nate@natejonah.com>
2019-03-01 00:24:31 +0000

    Remove render of html file

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const fetch = require("node-fetch");
 
-app.use(express.static("public"));
-
-app.get('/', (req, res) => res.render("index.html"));
+app.get('/', (req, res) => res.send("Quiz"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
