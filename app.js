081a73d232c2aa5dfa5e4ab20fecb424620bace9
Nate Jonah <nate@natejonah.com>
2019-03-10 23:42:28 +0000

    Set up root route to render index.html page

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const http = require("http");
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("yes"));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname + "/public/index.html")));
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}...`));
