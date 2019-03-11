b029506c7fd1a65e3ffddf26fb5675f22e2273b6
Nate Jonah <nate@natejonah.com>
2019-03-11 22:14:16 +0000

    Create route for compiled react js file

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname + "/public/index.html")));
 
+app.get('/index-jsx/', (req, res) => res.sendFile(path.resolve(__dirname + '/public/js/index.js')));
+
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}...`));
