5895f6a8b440a2516d57f2d577fe4758fa928d09
Nate Jonah <nate@natejonah.com>
2019-03-14 22:31:44 +0000

    Change index-js route file to bundle.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname + "/public/index.html")));
 
-app.get('/index-jsx/', (req, res) => res.sendFile(path.resolve(__dirname + '/public/js/index.js')));
+app.get('/index-jsx/', (req, res) => res.sendFile(path.resolve(__dirname + '/public/bundle.js')));
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}...`));
