4ee51853974e2dc0c2cd4f0e5a06401206f494c7
Nate Jonah <nate@natejonah.com>
2019-03-15 22:08:49 +0000

    Amend path to index.jsx

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { dest, parallel, series, src, watch } = require("gulp");
 const babel = require("gulp-babel");
 
 const transpile = () => {
-    return src("src/index.jsx")
+    return src("index.jsx")
         .pipe(babel({
             presets: ['@babel/env', '@babel/react'],
             plugins: ['transform-react-jsx']
