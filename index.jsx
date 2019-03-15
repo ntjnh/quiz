7c7b8f5347e7fdb8f7cdad52ab9c706bae54d5fc
Nate Jonah <nate@natejonah.com>
2019-03-15 22:06:26 +0000

    Move index jsx file to root and setup container div

---
+++ index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 id="title">Quiz</h1>
+
+                {/* Questions Component */}
+                {/* Submit button? */}
+                {/* Results modal */}
+            </div>
+        );
+    }
+}
+
+if (typeof window !== 'undefined') {
+    ReactDOM.render(<App />, document.getElementById("root"));
+}
