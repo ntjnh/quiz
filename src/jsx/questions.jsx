f11c7dfe6ce18302a840e30cf4b69dfa8363708c
Nate Jonah <nate@natejonah.com>
2019-03-16 23:48:31 +0000

    Fetch questions from the trivia API and render the results

--- src/jsx/questions.jsx
+++ src/jsx/questions.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 
 class Questions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            questions: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://opentdb.com/api.php?amount=20&type=multiple")
+        .then(res => res.json())
+        .then(result => {
+            this.setState({
+                isLoaded: true,
+                questions: result.results
+            });
+        },
+        error => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        });
+    }
+
     render() {
-        return <div>questions</div>;
+        const { error, isLoaded, questions } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if(!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    {questions.map((question, i) => (
+                        <div className="question" key={"question-" + (i + 1)}>
+                            <h3 className="question-text">{question.question}</h3>
+                        </div>
+                    ))}
+                </div>
+            );
+        }
     }
 }
 
